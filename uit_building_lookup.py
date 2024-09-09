ede503e21f02c61bf72aeaea92361f597aaf3284
N8Deathrider <nath001c@gmail.com>
2024-09-09 10:40:01 -0600

    Add uit_building_lookup.py script for building information lookup at UIT

---
+++ uit_building_lookup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+#TODO: Add description
+"""
+
+# Standard libraries
+import argparse
+import logging
+from sys import exit
+
+# Third-party libraries
+from rich.console import Console
+from rich.table import Table
+from rich.logging import RichHandler
+
+# Local libraries
+
+
+# Standard exit codes
+EXIT_SUCCESS = 0  # No errors
+EXIT_GENERAL_ERROR = 1  # General error
+EXIT_INVALID_ARGUMENT = 120  # Invalid argument to exit
+EXIT_KEYBOARD_INTERRUPT = 130  # Keyboard interrupt (Ctrl+C)
+
+
+# Logging setup
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler()]
+)
+log: logging.Logger = logging.getLogger("rich")
+
+
+def get_args() -> argparse.Namespace:
+    """
+    Parse command line arguments and return the parsed arguments.
+
+    Returns:
+        argparse.Namespace: The parsed command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="A script to lookup building information at UIT"
+    )
+
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help=argparse.SUPPRESS
+    )
+
+    parser.add_argument(
+        "building_number",
+        type=str,
+        help="The building number to lookup"
+        # TODO: look at adding more arguments so that multiple buildings can be looked up at once
+    )
+
+    return parser.parse_args()
+
+
+def main() -> None:
+    """
+    #TODO: Add description
+    """
+    ...
+
+
+if __name__ == "__main__":
+    try:
+        console = Console()
+        main()
+    except KeyboardInterrupt:
+        log.info("\nCtrl + c pressed. Exiting script...")
+        exit(EXIT_KEYBOARD_INTERRUPT)
+    except Exception as e:
+        log.exception(f"An unhandled error occurred: {e}")
+        exit(EXIT_GENERAL_ERROR)
